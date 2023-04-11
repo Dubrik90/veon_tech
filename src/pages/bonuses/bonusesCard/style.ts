f7ad3b5a9ff03e6d4f6258adad03f6400ff72ab7
Alexey <dubrik90@yandex.by>
2023-04-11 17:58:02 +0300

    refactor bonuse cards

--- src/pages/bonuses/bonusesCard/style.ts
+++ src/pages/bonuses/bonusesCard/style.ts
@@ -5,21 +5,40 @@ export const BonusesCardWrapper = styled.li`
   background: var(--colors-bg);
   box-shadow: 2px 4px 20px 2px rgba(0, 0, 0, 0.25);
   border-radius: 17px;
-  padding-top: calc(80px + (150 - 80) * ((100vw - 320px) / (1920 - 320)));
-  padding-bottom: calc(30px + (40 - 30) * ((100vw - 768px) / (1920 - 768)));
-  padding-left: calc(20px + (50 - 20) * ((100vw - 768px) / (1920 - 768)));
-  padding-right: calc(20px + (50 - 20) * ((100vw - 768px) / (1920 - 768)));
+  padding-top: calc(60px + (100 - 60) * ((100vw - 320px) / (1920 - 320)));
+  padding-bottom: calc(20px + (30 - 20) * ((100vw - 768px) / (1920 - 768)));
+  padding-left: calc(20px + (40 - 20) * ((100vw - 768px) / (1920 - 768)));
+  padding-right: calc(20px + (40 - 20) * ((100vw - 768px) / (1920 - 768)));
   position: relative;
   ${flexColumn()};
   z-index: 10;
-  max-width: 458px;
-  flex: 1 1 auto;
-  min-height: 388px;
+  //max-width: 450px;
+  // flex: 0 1 400px;
+  min-height: 350px;
+  flex: 0 1 450px;
   
-
+  
   @media (min-width: 769px) {
-    width: calc(33% - 10px);
+    :nth-child(1) {
+      grid-area: 1 / 1 / 2 / 2;
+    }
+
+    :nth-child(2) {
+      grid-area: 1 / 2 / 2 / 3;
+    }
+
+    :nth-child(3) {
+      grid-area: 1 / 3 / 2 / 4;
+    }
+
+    :nth-child(4) {
+      grid-area: 2 / 2 / 3 / 3;
+    }
   }
+
+  //@media (min-width: 769px) {
+  //  width: calc(33% - 10px);
+  //}
 
   span {
     content: '';
@@ -32,47 +51,57 @@ export const BonusesCardWrapper = styled.li`
     text-transform: lowercase;
     color: var(--colors-text-dark);
     transition: all 0.3s;
-    
+
     @media (max-width: 425px) {
       top: -16px;
-    //  left: 33px;
-    //  transform: translateX(0);
+      //  left: 33px;
+      //  transform: translateX(0);
       font-size: 20px;
     }
   }
-  
+
   :after {
     background: var(--colors-bg-green);
     content: '';
     position: absolute;
-    top: -32px;
+    top: -28px;
     left: 0;
     transform: translateX(-50%);
-    width: 65px;
-    height: 65px;
+    width: 50px;
+    height: 50px;
     border: 2px solid #5DB447;
     border-radius: 100%;
     z-index: -1;
-    
+
+    @media (max-width: 1024px) {
+      width: 45px;
+      height: 45px;
+      top: -25px;
+    }
+
     @media (max-width: 425px) {
       top: -23px;
-    //  left: 15px;
-   //   transform: translateX(0);
+      //  left: 15px;
+      //   transform: translateX(0);
       width: 40px;
       height: 40px;
     }
   }
-  
-  
+
+
 `
 export const ImgBlock = styled.div`
+  max-width: 50px;
   background: var(--colors-bg-grey-card-bonuse);
   position: absolute;
   top: -32px;
   left: 50%;
   transform: translateX(-50%);
   z-index: 10;
-  
+
+  @media (max-width: 1024px) {
+    max-width: 45px;
+  }
   @media (max-width: 425px) {
     max-width: 50px;
     top: -20px;
@@ -85,16 +114,21 @@ export const ImgBlock = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 110px;
-    height: 110px;
+    width: 90px;
+    height: 90px;
     border: 2px solid #5DB447;
     border-radius: 100%;
     z-index: -1;
-    
+
+    @media (max-width: 1024px) {
+      width: 85px;
+      height: 85px;
+    }
+
     @media (max-width: 425px) {
       width: 90px;
       height: 90px;
-      
+
     }
   }
 
@@ -104,26 +138,36 @@ export const Img = styled.img`
 `
 export const Title = styled.h5`
   font-weight: var(--fw-bold);
-  font-size: calc(16px + (32 - 16) * ((100vw - 320px) / (1920 - 320)));
+  font-size: calc(18px + (32 - 18) * ((100vw - 768px) / (1920 - 768)));
   line-height: 129.5%;
   text-align: center;
   color: var(--colors-text-dark);
+  @media (max-width: 768px) {
+    font-size: 16px
+  }
 `
 export const SubTitle = styled.p`
   font-weight: var(--fw-bold);
-  font-size: calc(14px + (20 - 14) * ((100vw - 320px) / (1920 - 320)));
+  font-size: calc(14px + (20 - 14) * ((100vw - 768px) / (1920 - 768)));
   line-height: 129.5%;
   text-align: center;
   color: var(--colors-text-dark);
-  margin-bottom: calc(20px + (70 - 20) * ((100vw - 320px) / (1920 - 320)));
+  margin-bottom: calc(20px + (40 - 20) * ((100vw - 320px) / (1920 - 320)));
+  
+  @media (max-width: 768px) {
+    font-size: 14px
+  }
 `
 export const Text = styled.p`
-  font-size: calc(14px + (17 - 14) * ((100vw - 320px) / (1920 - 320)));
+  font-size: calc(13px + (17 - 13) * ((100vw - 768px) / (1920 - 768)));
   line-height: 178.5%;
   text-align: center;
   letter-spacing: 0.005em;
   color: var(--colors-text-dark);
   margin-bottom: calc(20px + (30 - 20) * ((100vw - 320px) / (1920 - 320)));
+  @media (max-width: 768px) {
+    font-size: 14px
+  }
 `
 export const SubText = styled.p`
   flex: 1 1 auto;
@@ -131,7 +175,7 @@ export const SubText = styled.p`
   line-height: 129.5%;
   text-align: center;
   color: var(--colors-text-dark);
-  margin-bottom: calc(20px + (50 - 20) * ((100vw - 320px) / (1920 - 320)));
+  margin-bottom: calc(20px + (30 - 20) * ((100vw - 320px) / (1920 - 320)));
 `
 export const Button = styled.button`
   position: relative;
@@ -150,10 +194,10 @@ export const Button = styled.button`
   text-transform: uppercase;
   z-index: 10;
 
-  @media(max-width: 1024px) {
+  @media (max-width: 1024px) {
     align-self: stretch;
   }
-  
+
 
   //@media (max-width: 980px) {
   //  align-self: stretch;
--- src/pages/bonuses/style.ts
+++ src/pages/bonuses/style.ts
@@ -5,16 +5,21 @@ export const BonusesWrapper = styled.div`
   background: var(--colors-bg-green-dark);
 `
 export const BonusesCardsBlock = styled.ol`
-  // display: grid;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+
   // grid-template-columns: repeat(auto-fit, minmax(250px, 458px));
-  display: flex;
-  flex-wrap: wrap;
+  // display: flex;
+  // flex-wrap: wrap;
   justify-content: center;
-  column-gap: 50px;
+  max-width: 1490px;
+  margin: 0 auto;
+  column-gap: calc(15px + (50 - 15) * ((100vw - 768px) / (1920 - 768)));
   //column-gap: calc(20px + (50 - 20) * ((100vw - 768px) / (1920 - 768)));
   row-gap: 80px;
-  padding-top: calc(100px + (200 - 100) * ((100vw - 320px) / (1920 - 320)));
-  padding-bottom: calc(50px + (100 - 50) * ((100vw - 320px) / (1920 - 320)));
+  padding-top: calc(100px + (200 - 100) * ((100vw - 768px) / (1920 - 768)));
+  padding-bottom: calc(50px + (100 - 50) * ((100vw - 768px) / (1920 - 768)));
 
   @media (max-width: 768px) {
     display: none;
