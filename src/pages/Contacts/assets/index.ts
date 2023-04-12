4a2063592708d1ff2c693ce642550efdfd9d442a
alex <Dubik90@yandex.by>
2023-04-12 23:59:18 +0300

    refactor style modal window and form bonuse user

--- src/pages/Contacts/assets/index.ts
+++ src/pages/Contacts/assets/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as LocationIcon } from './location.svg';
 export { ReactComponent as EmailIcon } from './email.svg';
 export { ReactComponent as PhoneIcon } from './phone.svg';
+export { ReactComponent as FaceBook } from './Vector.svg';
