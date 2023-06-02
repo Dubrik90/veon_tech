fddfd80d248447396eaea47816ab31b9d5f9a54f
Alexey <dubrik90@yandex.by>
2023-06-02 13:15:39 +0300

    refactor veon_com

--- src/index.tsx
+++ src/index.tsx
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <Provider store={store}>
         <GlobalStyles/>
-        <HashRouter>
+        <BrowserRouter>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
 );
 
