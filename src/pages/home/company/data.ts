4e7de6444a0e96f0f2fc89d2eee35a5e3ce9ecc4
alexey <dubrik90@yandex.by>
2023-06-29 09:24:15 +0300

    refactor logo company data

--- src/pages/home/company/data.ts
+++ src/pages/home/company/data.ts
@@ -1,84 +1,61 @@
-import rent from "./assets/rent.png";
-import rentWite from "./assets/rentWite.png";
-import sodox from "./assets/sodox.png";
-import granit from "./assets/granit.png";
-import granitWite from "./assets/granitWite.png";
-import noomblisting from "./assets/noomblisting.png";
-
-
 import noomb from "./assets/noombN.png";
 import nuahuleN from "./assets/nuahuleN.png";
 import opticsN from "./assets/opticsN.png";
 import partyN from "./assets/partyN.png";
 import prosN from "./assets/prosN.png";
-
-import sodox2 from "./assets/sodox2.png";
-
-
-import sodoxWite from "./assets/sodoxWite.png";
-import sodoxWite2 from "./assets/sodoxWite.png";
-import noomblistingWite from "./assets/noomblistingWite.png";
+import sodoxN from "./assets/sodoxN.png";
 
 export type ClientsType = {
     img: string,
-    imgWhite: string
 }
 
 export const clients: ClientsType[] = [
     {
         img: prosN,
-        imgWhite: rentWite
     },
     {
         img: partyN,
-        imgWhite: sodoxWite
     },
     {
         img: opticsN,
-        imgWhite: granitWite
     },
     {
         img: nuahuleN,
-        imgWhite: noomblistingWite
     },
     {
         img: prosN,
-        imgWhite: rentWite
+    },
+    {
+        img: sodoxN,
     },
     {
         img: partyN,
-        imgWhite: sodoxWite
     },
     {
         img: opticsN,
-        imgWhite: granitWite
     },
     {
         img: nuahuleN,
-        imgWhite: noomblistingWite
     },
     {
         img: noomb,
-        imgWhite: sodoxWite2
     },
     {
         img: prosN,
-        imgWhite: rentWite
+    },
+    {
+        img: sodoxN,
     },
     {
         img: partyN,
-        imgWhite: sodoxWite
     },
     {
         img: opticsN,
-        imgWhite: granitWite
     },
     {
         img: nuahuleN,
-        imgWhite: noomblistingWite
     },
     {
         img: noomb,
-        imgWhite: sodoxWite2
     },
 ]
